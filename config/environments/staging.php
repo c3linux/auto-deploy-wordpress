0e788803de8bbc7c2e99b2cd62afc63feb171307
Elmar <61630050+c3linux@users.noreply.github.com>
2021-01-28 08:27:02 +0400

    Update staging.php

--- config/environments/staging.php
+++ config/environments/staging.php
@@ -4,8 +4,7 @@
  */
 
 use Roots\WPConfig\Config;
-Config::define('WP_HOME', 'https://auto-deploy-wordpress-gbz22.ondigitalocean.app');
-Config::define('WP_SITEURL', 'https://auto-deploy-wordpress-gbz22.ondigitalocean.app/wp');
+
 /**
  * You should try to keep staging as close to production as possible. However,
  * should you need to, you can always override production configuration values
